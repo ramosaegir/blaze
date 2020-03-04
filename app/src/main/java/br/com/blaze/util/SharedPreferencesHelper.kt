c2ccf86f4b9de22d1ed2d74b53f39d11bc768026
Fabricio Ramos <fabricioramoscosta@gmail.com>
2020-03-04 01:30:31 -0300

    Changes in Welcome Activity

--- app/src/main/java/br/com/blaze/util/SharedPreferencesHelper.kt
+++ app/src/main/java/br/com/blaze/util/SharedPreferencesHelper.kt
@@ -7,8 +7,4 @@ import br.com.blaze.view.welcome.WelcomeActivity
 
 class SharedPreferencesHelper {
 
-    lateinit var prefs: SharedPreferences
-
-    fun getFirstAccess(): Boolean = prefs.getBoolean(PreferenceKeys.PREFS_IS_FIRST_ACCESS, true)
-
 }
--- app/src/main/java/br/com/blaze/view/splash/SplashActivity.kt
+++ app/src/main/java/br/com/blaze/view/splash/SplashActivity.kt
@@ -1,18 +1,13 @@
 package br.com.blaze.view.splash
 
-import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import br.com.blaze.R
-import br.com.blaze.constants.PreferenceKeys
-import br.com.blaze.util.SharedPreferencesHelper
 import br.com.blaze.view.welcome.WelcomeActivity
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
-
-    private val sharedPreferencesHelper = SharedPreferencesHelper()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,28 +29,10 @@ class SplashActivity : AppCompatActivity() {
     fun setSplashHandler(){
         Handler().postDelayed({
 
-            isFirstAccess()
-
             startActivity(WelcomeActivity.newInstance(this))
             //finish this activity
             finish()
         },3000)
-    }
-
-    fun isFirstAccess(){
-        if(sharedPreferencesHelper.getFirstAccess()){
-            callWelcome()
-        }else{
-            callAuthentication()
-        }
-    }
-
-    fun callWelcome(){
-        startActivity(WelcomeActivity.newInstance(this))
-    }
-
-    fun callAuthentication(){
-        print("Authentication triggered")
     }
 
 }
